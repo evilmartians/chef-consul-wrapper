4ac3db13679f810db5e919447e23e158069f006b
Kirill Kouznetsov <agon.smith@gmail.com>
2017-01-19 19:13:55 +0300

    recipe[consul_wrapper::node_exporter] was added.

---
+++ recipes/node_exporter.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: consul_wrapper
+# Recipe:: node_exporter
+#
+# Copyright 2017, Evil Martians
+#
+# All rights reserved - Do Not Redistribute
+#
+
+unless node['consul_wrapper']['disable']
+  interface = node['consul_wrapper']['listen_interface']
+  ip = '127.0.0.1'
+  ip = node['network']['interfaces'][interface]['addresses'].find { |address, data| data['family'] == 'inet' }.first if node['network']['interfaces'].key?(interface)
+
+  consul_wrapper_node_exporter 'main' do
+    address ip
+  end
+end
