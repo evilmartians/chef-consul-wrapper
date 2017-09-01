42faddab380a332041b2aed6203058e9236ed900
Kirill Kouznetsov <agon.smith@gmail.com>
2017-09-01 17:59:52 +0800

    Rails service check were refactored too.
    We removed http check because we usually use unix socket for a rail app. And it is already checked by `nc`-type process check.

--- resources/rails.rb
+++ resources/rails.rb
@@ -9,12 +9,12 @@
 
 property :service_name, String, name_property: true
 property :address, String, default: '127.0.0.1'
-property :socket, [String, Fixnum, FalseClass], default: false
+property :socket, [String, Integer, FalseClass], default: false
 property :url, String, default: '/'
 property :hostname, String, default: 'example.com'
 property :username, String, default: 'root'
 property :appserver, String, default: 'unicorn'
-property :min_procs, Fixnum, default: 2
+property :min_procs, Integer, default: 2
 property :tags, Array, default: []
 
 default_action :add
@@ -26,7 +26,7 @@ action :add do
 
   proc_lower_limit = min_procs
 
-  nc_cmd = if socket.is_a?(Fixnum) || /\A\d+\z/.match(socket)
+  nc_cmd = if socket.is_a?(Integer) || /\A\d+\z/.match(socket)
              "/bin/nc #{address} #{socket}"
            else
              "/bin/nc -U #{socket}"
@@ -62,18 +62,6 @@ action :add do
       script: "/bin/echo -e 'GET #{url} HTTP/1.1\\r\\nHost: #{hostname}\\r\\nConnection: close\\r\\n\\r\\n' | #{nc_cmd} | /bin/grep 'HTTP/1.1 200 OK'",
       interval: '15s',
       notes: "#{service_name.capitalize} rails should serve requests on its socket.",
-      service_id: "application-#{service_name}"
-    )
-    notifies :reload, 'consul_service[consul]'
-  end
-
-  consul_definition "app-#{service_name}-http-port-check" do
-    type 'check'
-    parameters(
-      tcp: "#{address}:80",
-      interval: '15s',
-      timeout: '1s',
-      notes: "NGINX should listen on #{address}:80",
       service_id: "application-#{service_name}"
     )
     notifies :reload, 'consul_service[consul]'
